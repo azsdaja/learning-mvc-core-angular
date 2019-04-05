144ccf43755b5288cb78d3e9e53e2e504514e38a
pawel <pawel_s1@o2.pl>
2019-04-05 13:15:55 +0200

    3/29 dodano wiązanie wstrzykiwania zależności dla IAuthRepository

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -30,6 +30,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>(); // bo Singleton mógłby powodować problemy przy równoległych zapytaniach
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
