9751821126b840825429d1ffff71f54b77161c25
pawel <pawel_s1@o2.pl>
2019-04-05 13:11:58 +0200

    dodano model użytkownika oraz ich repozytorium, które m.in. sprawdza poprawność danych logowania z użyciem skrótowania

---
+++ DatingApp.API/Data/AuthRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<User> LoginAsync(string username, string password)
+        {
+            User user = await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+
+            if (user == null)
+                return null;
+
+            if(!PasswordIsCorrect(password, user.PasswordSalt, user.PasswordHash))
+            {
+                return null;
+            }
+
+            return user;
+        }
+
+        private bool PasswordIsCorrect(string password, byte[] passwordSalt, byte[] passwordHash)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            HashAndSalt(password, out passwordHash, out passwordSalt);
+            user.PasswordSalt = passwordSalt;
+            user.PasswordHash = passwordHash;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void HashAndSalt(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        
+        public async Task<bool> UserExistsAsync(string username)
+        {
+            return await _context.Users.AnyAsync(u => u.Username == username);
+        }
+    }
+}
